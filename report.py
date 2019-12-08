252f3dc529fe18870493e207c25c1d02f2d9b57f
Flip Jooste <flip@profmap.com>
2019-12-08 17:42:44 +0200

    new

--- report.py
+++ report.py
@@ -50,3 +50,36 @@ cafile = 'cacert.pem'
 bld = 'https://db.profmapcloud.com/ords/PDB1/dpw/buildings/2013'
 bldresponse = requests.get(bld,verify=False)
 blddata = bldresponse.json()
+
+#%%
+from reportlab.pdfgen import canvas
+ def hello(c):
+ c.drawString(100,100,"Hello Flip World")
+ c = canvas.Canvas("helloflip.pdf")
+ hello(c)
+ c.showPage()
+ c.save()
+
+#%%
+
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+
+canvas = canvas.Canvas("form.pdf", pagesize=letter)
+canvas.setLineWidth(.3)
+canvas.setFont('Helvetica', 12)
+
+canvas.drawString(30, 750, 'OFFICIAL COMMUNIQUE')
+canvas.drawString(30, 735, 'OF ACME INDUSTRIES')
+canvas.drawString(500, 750, "12/12/2010")
+canvas.line(480, 747, 580, 747)
+
+canvas.drawString(275, 725, 'AMOUNT OWED:')
+canvas.drawString(500, 725, "$1,000.00")
+canvas.line(378, 723, 580, 723)
+
+canvas.drawString(30, 703, 'RECEIVED BY:')
+canvas.line(120, 700, 580, 700)
+canvas.drawString(120, 703, "JOHN DOE")
+
+canvas.save()
